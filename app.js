23e69f5a0d7aacd4f8f84a989d3a6bab61b91eed
Gerardo <agerardo1397@gmail.com>
2020-11-05 21:22:13 -0600

    modal product working

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const hamburger = document.querySelector('.hamburger img');
 const menu = document.querySelector('.right');
 const close_menu = document.querySelector('.close-menu');
 //document.getElementById("img1").style.zIndex = "1";
-
-const btn = document.querySelector('#btn-aux');
-const form = document.querySelector('.form');
+let product_selected = '';
+const modal = document.querySelector('.modal');
+const services_type = document.querySelectorAll('.product-item');
+const postProduct_btn = document.querySelector('.postProduct-btn');
+const sendInfo_btn = document.querySelector('.sendInfo-btn');
+const product_btn = document.querySelector('.landing__cta');
+const closeModal = document.querySelector('.closeModal');
 
 window.addEventListener('resize', function (e) {
     if (window.innerWidth > 810) {
@@ -20,7 +24,38 @@ hamburger.addEventListener('click', function (e) {
 close_menu.addEventListener('click', function (e) {
     menu.style.transform = 'translateX(50vw)';
 });
-btn.addEventListener('click', function (e) {
-    btn.style.display = 'none';
-    form.style.display = 'flex';
+closeModal.addEventListener('click', function (e) {
+    document.querySelector('.products__select').style.display = 'flex';
+    document.querySelector('.modal-wrapper').style.display = 'none';
+    document.querySelector('.form-postProduct').style.display = 'none';
+    document.querySelector('.form-sendInfo').style.display = 'none';
+});
+product_btn.addEventListener('click', function (e) {
+    document.querySelector('.modal-wrapper').style.display = 'flex';
+});
+
+for (let index = 0; index < services_type.length; index++) {
+    services_type[index].addEventListener('click', function (e) {
+        document.querySelector('.main_title').style.display = 'none';
+        document.querySelector('.quote').style.display = 'flex';
+        document.querySelector('.products__select').style.display = 'none';
+        document.querySelector('.form-postProduct').style.display = 'flex';
+        modal.style.height = '70vh';
+        // obtiene el dataset el produto seleccionado
+        product_selected = services_type[index].dataset.type;
+        console.log(product_selected);
+    });
+}
+
+postProduct_btn.addEventListener('click', function (e) {
+    document.querySelector('.form-postProduct').style.display = 'none';
+    // DEBE GUARDAR LOS DATOS CAPTURAS EN EL FORM ANTERIOR PARA PODERLE ENVIAR LA INFO A EL TRABAJADOR
+    document.querySelector('.form-sendInfo').style.display = 'flex';
+    modal.style.height = '75vh';
+    document.querySelector('#product').value = product_selected.toUpperCase();
+    document.querySelector('#product').disabled = true;
+});
+
+sendInfo_btn.addEventListener('click', function (e) {
+    document.querySelector('.modal-wrapper').style.display = 'none';
 });
